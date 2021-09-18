d0326c25af73989449a99946dc7d076c8d72fb6d
sushant-rangari <90927264+sushant-rangari@users.noreply.github.com>
2021-09-18 20:08:21 +0530

    added print statement

--- src/com/projectB/ProjectB_HelloWorld.java
+++ src/com/projectB/ProjectB_HelloWorld.java
@@ -5,6 +5,8 @@ public class ProjectB_HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Project B Hello World");
+		
+		System.out.println("Added another line in ProjectB Hello World");
 
 	}
 
