7bb6bbc190472af334dc8d516055e6afcd57249d
sushant-rangari <ntechsushant@yahoo.co.in>
2021-09-19 16:25:07 +0530

    sum logic is added from Feature_Second_Class branch

--- src/com/projectB/SecondClass_ProjectB.java
+++ src/com/projectB/SecondClass_ProjectB.java
@@ -5,6 +5,10 @@ public class SecondClass_ProjectB {
 	public static void main(String[] args) {
 		
 		System.out.println("Added another class");
+		
+		int a = 10, b = 20;
+		
+		System.out.println("The sum of A and B is "+ a+b);
 
 	}
 
