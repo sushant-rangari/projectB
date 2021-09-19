446574c4c8d351870fce01aaa5a6a9f449d73a60
sushant-rangari <90927264+sushant-rangari@users.noreply.github.com>
2021-09-19 16:27:14 +0530

    Update SecondClass_ProjectB.java

--- src/com/projectB/SecondClass_ProjectB.java
+++ src/com/projectB/SecondClass_ProjectB.java
@@ -5,6 +5,8 @@ public class SecondClass_ProjectB {
 	public static void main(String[] args) {
 		
 		System.out.println("Added another class");
+		
+		System.out.println("Logic of summations needs to be done below");
 
 	}
 
